6d27b739e589f126cdd846742d1937827a124d00
Arthur Corenzan <arthur@corenzan.com>
2012-08-16 14:16:36 -0300

    update commentary

--- ko.resource.js
+++ ko.resource.js
@@ -1,48 +1,60 @@
 /*
- * Knockout Resource v0.2 2012-06-01 10:13:24 -0300
+ * Knockout Resource v0.3 2012-08-16 13:31:13 -0300
  * by Arthur Corenzan <arthur@corenzan.com>
  * licensed under http://creativecommons.org/licenses/by/3.0
- * more on http://haggen.github.com/ko.resource
+ * more on http://github.com/haggen/ko.resource
  */
 (function($, ko, undefined) {
+  var request;
 
-    // Shortcut for restful JSON requests
-    $.rest = function(method, url, data, callback) {
-      var settings = {
-        url: url,
-        data: data,
-        type: method,
-        contentType: 'application/json',
-        success: callback
-      };
-
-      // console.log('Restful:', settings);
-
-      $.ajax(settings);
+  // RESTful request shortcut
+  request = function(method, url, data, callback) {
+    var settings = {
+      url: url,
+      data: data,
+      type: method,
+      success: callback
     };
 
+    settings.complete = function() {
+      console.log('Request:', settings, this);
+    };
+
+    $.ajax(settings);
+  };
+
   // Resource constructor
-  // path: string, path prefix to where this resource can be found on your API
-  // schema: hash, default values for observable attributes, functions will be set as computed values
-  // arbitrary: hash, arbitrary properties and methods (not observables)
+  // path: string, path to the resource on your RESTful API
+  // schema: optional hash, your model's obervable attributes (functions will be considered computed values)
+  // arbitrary: optional hash, arbitrary properties and methods of your model (not observables)
   function Resource(path, schema, arbitrary) {
     var resource;
 
-    if(arbitrary === undefined) {
-      arbitrary = schema;
+    if(schema === undefined) {
       schema = {};
+    }
+
+    if(arbitrary === undefined) {
+      arbitrary = {};
     }
 
     resource = function(source) {
       var instance = this;
 
+      // Data source, may be a JSON string os JS Object
+      if(source === undefined) {
+        source = {};
+      }
+
+      // Action hooks
+      // TODO: replace with callbacks within every action
       instance.onSave = $.noop;
       instance.onFetch = $.noop;
       instance.onDestroy = $.noop;
 
       // Attach arbitrary properties and methods
       $.each(arbitrary, function(name, value) {
-        if(typeof value === 'function') {
+        if(value instanceof Function) {
           instance[name] = function() {
             value.apply(instance, arguments);
           };
@@ -51,26 +63,35 @@
         }
       });
 
-      // Make our own serializer so we can exclude attributes started with double underscore
+      // Recursively serialize your model, also excludes attributes started with double underscore
       instance.serialize = function() {
         var data = ko.toJS(instance);
 
         $.each(data, function(name, value) {
-          if(/^__/.exec(name) || typeof value === 'function') {
+          if(name.indexOf('__') === 0 || value instanceof Function) {
             delete data[name];
+          } else {
+            if(value instanceof Array) {
+              $.each(value, function(i, v) {
+                if('serialize' in v) {
+                  value[i] = v.serialize();
+                }
+              });
+            }
           }
         });
 
-        return JSON.stringify(data);
+        return data;
       };
 
-      // Attribute setter
-      instance.set = function(attributes) {
+      // Update your model's observable attributes
+      // Originally this method was called `set` but it can conflict with attribute name
+      instance.update_attributes = function(attributes) {
         $.each(attributes, function(attr, value) {
-          if(typeof instance[attr] === 'undefined') {
-            if(typeof value === 'function') {
+          if(instance[attr] === undefined) {
+            if(value instanceof Function) {
               instance[attr] = ko.computed(value, instance);
-            } else if(typeof value === 'array') {
+            } else if(value instanceof Array) {
               instance[attr] = ko.observableArray(value);
             } else {
               instance[attr] = ko.observable(value);
@@ -81,34 +102,51 @@
         });
       };
 
-      // Save object's data, using PUT if it got an id, or POST otherwise
+      // Send model's data to the server, auto-switching between POST and PUT
       instance.save = function() {
-        if(instance._id) {
-          $.rest('put', path + '/' + instance._id(), instance.serialize(), instance.onSave);
+        if('_id' in instance) {
+          request('put', path + '/' + instance._id(), instance.serialize(), instance.onSave);
         } else {
-          $.rest('post', path, instance.serialize(), function(response) {
-            instance.set({ _id: response._id });
+          request('post', path, instance.serialize(), function(response) {
+            // We assume your're using Mongo and that the response comes with an ID
+            instance.update_attributes({ _id: response._id });
             instance.onSave(response);
           });
         }
       };
 
-      // Fetch instance from server, but only works if it has an attribute _id
-      // TODO: add some check for id existance
+      // Fetch model's data from the server
+      // TODO: check for instance's _id
       instance.fetch = function() {
-        $.rest('get', path + '/' + instance._id(), null, function(response) {
-          instance.set(response);
+        request('get', path + '/' + instance._id(), null, function(response) {
+          instance.update_attributes(response);
           instance.onFetch(response);
         });
       };
 
-      // Delete object from server
+      // Destroy object from the server but retains current model data
       instance.destroy = function() {
-        $.rest('delete', path + '/' + instance._id(), instance.onDestroy);
+        request('delete', path + '/' + instance._id(), instance.onDestroy);
       };
 
-      instance.set(schema);
-      instance.set(source);
+      // Request for custom actions
+      instance.request = function(method, action, data, callback) {
+        if(callback === undefined) {
+          callback = data;
+          data = {};
+        }
+
+        request(method, path + action, data, function(response) {
+          callback.apply(instance, [response]);
+        });
+      };
+
+      instance.update_attributes(schema);
+      instance.update_attributes(source instanceof String ? JSON.parse(source) : source);
+
+      if(instance.initialize !== undefined) {
+        instance.initialize(source);
+      }
     };
 
     // Fetch a collection of objects
@@ -118,7 +156,7 @@
         query = {};
       }
 
-      $.rest('get', path, query, function(response) {
+      request('get', path, query, function(response) {
         callback($.map(response, function(source) {
           return new resource(source);
         }));
@@ -128,7 +166,7 @@
     return resource;
   }
 
-  // Expose as ko plugin
+  // Expose the plugin
   ko.resource = Resource;
 
 })(window.jQuery, window.ko);
